f57b9b4faed441c46f3899072acf50391b573484
AbhinRustagi <rustagi.abhin@gmail.com>
2025-03-12 19:55:24 +1100

    feat: Add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,5 +6,29 @@ export const metadata = generateMetadata({
 });
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-4 max-w-md">
+        <span className="text-neutral-100">Software Engineer</span> with product
+        intuition and acute curiosity.
+      </h1>
+      <p className="mb-4 max-w-md">
+        I was born and raised in New Delhi. Currently I reside in Melbourne,
+        Australia. Prior to moving to Melbourne in 2023, I worked with various
+        small and mid-size teams as a software engineer. I have worked at
+        Openhouse, ReWorld Earth, Wetlands International, Virtetic, and more.
+      </p>
+      <p className="mb-4 max-w-md">
+        I migrated to Melbourne to study a Masters of IT, and specialize in
+        Artificial Intelligence. I completed a Bachelor of Science in Statistics
+        from University of Delhi before this, sharpening my analytical and
+        critical skills.
+      </p>
+      <p className="mb-4 max-w-md">
+        When I'm not working, you can usually find me reading up on variety of
+        things (astrophysics, cricket, finance, ∞), going for hikes, or
+        photographing natural landscapes.
+      </p>
+    </section>
+  );
 }
