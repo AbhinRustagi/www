f0a0f7189c10e3483882b9bd2d8cbcfe03a506c8
AbhinRustagi <rustagi.abhin@gmail.com>
2025-03-12 20:30:47 +1100

    feat: add home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Something</div>;
+  return (
+    <section>
+      <h1 className="font-bold text-neutral-100 text-5xl mb-4">
+        Hi, I&apos;m Abhin!
+      </h1>
+      <p className="text-3xl font-bold text-neutral-400 max-w-xl leading-normal">
+        Full Stack Engineer building user-first applications that help everyone.
+        Based in Melbourne.
+      </p>
+    </section>
+  );
 }
