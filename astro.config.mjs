a67b57d808c7e10a768c56701d8098aae611abaa
AbhinRustagi <rustagi.abhin@gmail.com>
2026-01-09 20:35:44 +1100

    fix: add wakatime & github contrib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://abhin.dev',
+  site: "https://abhin.dev",
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
