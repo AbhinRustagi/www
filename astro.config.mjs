649e1531ec14f1e6efbec4eddb0c88bd19f6b041
AbhinRustagi <rustagi.abhin@gmail.com>
2026-01-11 17:19:31 +1100

    fix: icons & styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: "https://abhin.dev",
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+    edgeMiddleware: true,
+  }),
 });
